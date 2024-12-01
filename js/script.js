f6ca3ee12b4c4671a881bd3f92427a83db766675
tiago <ti.almeida58@gmail.com>
2024-12-01 15:55:05 -0300

    feat: creating typed text

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Físico Fitness', 'Ganho de Massa', 'Treinamento de Força', 'Perder Gordura', 'Levantamento de Peso', 'Correndo'],
+    typeSpeed: 60,
+    backSpeed: 60,
+    backDelay: 1000,
+    loop: true,
+});
